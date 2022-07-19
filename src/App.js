a0c805c806419e5910935f77563677ff3dda65f3
Aditya <aditya26sg@gmail.com>
2022-07-20 01:58:22 +0530

    added flex display for address and content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [userAddress, setUserAddress] = useState("");
+
+  const connectWallet = async () => {
+    setWalletConnected(true);
+    console.log(walletConnected);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">SubVid</span>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <Modal show={!walletConnected}>
+          <Modal.Header closeButton>
+            <Modal.Title>Connect wallet</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Button variant="primary" onClick={() => connectWallet()}>
+              Connect Wallet
+            </Button>
+          </Modal.Body>
+        </Modal>
+
+        <div className='d-flex flex-row justify-content-around'>
+          <div className='addressList'>Address list</div>
+          <div className='contentList flex-grow-1'>Content List</div>
+        </div>
+
+      </div>
     </div>
   );
 }
