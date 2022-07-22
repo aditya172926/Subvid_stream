89e5a9f5701677138025471844af09b51a227778
Aditya <aditya26sg@gmail.com>
2022-07-22 22:45:13 +0530

    added connect wallet and spinner

--- src/App.js
+++ src/App.js
@@ -8,27 +8,45 @@ import Web3 from 'web3';
 import { newKitFromWeb3 } from '@celo/contractkit';
 import BigNumber from 'bignumber.js';
 
-const ERC20_DECIMALS = 20;
+const ERC20_DECIMALS = 18;
+
+var kit;
 
 function App() {
   const [walletConnected, setWalletConnected] = useState(false);
   const [userAddress, setUserAddress] = useState("");
+  const [userBalance, setUserBalance] = useState(0.0);
+  const [loadingSpinner, setLoadingSpinner] = useState(false);
 
   const connectWallet = async () => {
     if (window.celo) {
       console.log("Please approve this dapp to use it");
       try {
+        setLoadingSpinner(true);
         await window.celo.enable();
         const web3 = new Web3(window.celo);
-        let kit = newKitFromWeb3(web3);
+        kit = newKitFromWeb3(web3);
+        const accounts = await kit.web3.eth.getAccounts();
+        kit.defaultAccount = accounts[0];
+        setUserAddress(accounts[0]);
       } catch (error) {
         console.log(error);
+        setLoadingSpinner(false);
       }
     } else {
-      console.log("Install Celo Extension wallet")
+      console.log("Install Celo Extension wallet");
     }
+    await getBalance();
     setWalletConnected(true);
+    setLoadingSpinner(false);
     console.log(walletConnected);
+  }
+
+  const getBalance = async () => {
+    const totalBalance = await kit.getTotalBalance(kit.defaultAccount);
+    console.log(totalBalance);
+    const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2);
+    setUserBalance(cUSDBalance);
   }
 
   const sampleArray = [
@@ -51,23 +69,28 @@ function App() {
       <nav className="navbar bg-light">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1">SubVid</span>
+          Balance - {userBalance}
         </div>
       </nav>
       <div className="container-fluid">
         <Modal show={!walletConnected} onClick={() => connectWallet()} size="sm" centered>
-            <Button variant='light'>
-              <div className='text-center'>
-                <div className='logo mb-4 mt-4'>
-                  <img src={celo_logo} alt='celo logo' width={50} height={50} />
-                </div>
 
-                <div className='mb-4'>
-                    Connect Wallet
-                </div>
+          <Button variant='light'>
+            <div className='text-center'>
+              <div className='logo mb-4 mt-4'>
+                <img src={celo_logo} alt='celo logo' width={50} height={50} />
               </div>
-            </Button>
+              {!loadingSpinner ? (
+                <div className='mb-4'>
+                  Connect Wallet
+                </div>) : (
+                <div class="spinner-border" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
+              )}
+            </div>
+          </Button>
         </Modal>
-
         <div className='d-flex flex-row justify-content-around'>
           <div className='addressList'>
             <h3>Address List</h3>
@@ -79,7 +102,9 @@ function App() {
               })}
             </ul>
           </div>
-          <div className='contentList flex-grow-1'>Content List</div>
+          <div className='contentList flex-grow-1'>
+            Connected Wallet
+          </div>
         </div>
 
       </div>
